ea16858e97feb96d78c28168023efd0c1a58c0eb
divrawat <divrawat2001@gmail>
2024-06-13 19:46:41 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,553 @@
+
+export const DOMAIN = 'http://localhost:3000';
+// export const DOMAIN = 'https://www.vinlandsagamanga.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>Mushoku Tensei: Jobless Reincarnation</u></a> is a Japanese light novel series about a 34-year-old NEET who, after dying in a traffic accident, is reincarnated in a fantasy world as Rudeus Greyrat. With his past life's knowledge, determination, and newfound magical abilities, he strives to live a fulfilling life and avoid repeating his past mistakes. The story explores themes of redemption, personal growth, and the complexities of relationships in a richly detailed world.`
+
+
+export const MANGA_NAME = 'Mushoku Tensei';
+export const MANGA_AUTHOR = 'Rifujin na Magonote';
+export const MANGA_RELEASE = '2012';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Rifujin';
+export const MANGA_STUDIO = 'Studio Bind';
+export const MANGA_GENRE = 'Adventure, Drama, Comedy, Adult, Fantasy, Romance';
+
+export const CHAPTER_PREFIX = 'mushoku-tensei-chapter';
+export const IMAGE_PREFIX = 'images/mushoku-tensei';
+export const NEXT_PREVIOUS_PREFIX = 'manga/mushoku-tensei-chapter';
+export const URL_PREFIX = 'manga/mushoku-tensei-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/mushoku-tensei-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/12/Mushoku-Tensei-Cover-Art-That-Includes-Roxy-Eris-Rudeus-And-Sylphie.jpg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[70px] w-[100px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Mushoku Tensei';
+export const NavbarName = "Mushoku Tensei";
+
+export const APP_DESCRIPTION = `Read "${MANGA_NAME}" manga online at "${DOMAIN}". Enjoy high-quality scans, latest chapters, and connect with fans in our vibrant community.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "Mushoku Tensei: Jobless Reincarnation is a Japanese light novel series written by Rifujin na Magonote. It centers around the life of a 34-year-old unemployed man, who, after dying in a tragic traffic accident, finds himself reincarnated in a fantastical world of magic and swordplay. Reborn as Rudeus Greyrat, he retains all the memories of his past life, which deeply influence his actions and decisions in his new life."
+    },
+    {
+        id: 2,
+        content: "Determined not to repeat the mistakes of his former existence, Rudeus vows to live his new life to the fullest. He begins his journey as an infant in a world brimming with possibilities, where magic and mythical creatures are real. With his mature mind, he quickly masters complex magical skills and becomes proficient in various disciplines far beyond his years. His extraordinary talent attracts the attention of powerful figures and mentors who help him hone his abilities."
+    },
+    {
+        id: 3,
+        content: "Rudeus's journey is marked by numerous adventures and encounters with diverse characters. From forming deep bonds with family members like his father Paul, a skilled swordsman, and his mother Zenith, a compassionate healer, to making lifelong friends like the fierce warrior Eris Boreas Greyrat and the mysterious mage Roxy Migurdia, his life is a tapestry of relationships that shape his growth."
+    },
+    {
+        id: 4,
+        content: "Throughout the story, Rudeus grapples with the traumas of his past life, striving for personal redemption and self-improvement. His journey is not just about becoming stronger physically and magically but also about maturing emotionally and morally. He learns valuable lessons about love, friendship, and responsibility, facing both external threats from monsters and dark forces and internal struggles with his own flaws and insecurities."
+    },
+    {
+        id: 5,
+        content: "Mushoku Tensei delves deeply into themes of second chances, the importance of perseverance, and the complexity of human emotions. The narrative balances action-packed sequences with introspective moments, providing a well-rounded exploration of Rudeus's development. The richly constructed world, intricate character dynamics, and the protagonist's relentless pursuit of a meaningful life make 'Mushoku Tensei: Jobless Reincarnation' a compelling and thought-provoking tale of growth and redemption."
+    },
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+export const RelatedMangaLinks = [
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran',
+        link: 'https://www.frieranmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/one-punch-man.webp`,
+        title: 'One Punch Man',
+        link: 'https://www.onepunchmanmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/vinland-saga.webp`,
+        title: 'Vinland Saga',
+        link: 'https://www.vinlandsagamanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/jujutsu-kaisen.webp`,
+        title: 'Jujutsu Kaisen',
+        link: 'https://www.jujutsukaisenmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/black-clover.webp`,
+        title: 'Black Clover',
+        link: 'https://www.blackclovermanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/chainsaw-man.webp`,
+        title: 'Chainsaw Man',
+        link: 'https://www.chainsawmanmanga.in.net',
+    },
+    {
+        imageUrl: `${DOMAIN}/related/mushoku-tensei.webp`,
+        title: 'Mushoku Tensei',
+        link: 'https://www.mushokutenseimanga.in.net',
+    },
+];
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 38
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 41
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 35
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 37
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "10a",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 37
+    },
+    {
+        "chapterNumber": "16a",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 35
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "21a",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 45
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 35
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "56a",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 35
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 46
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 46
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "62",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "63",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "63a",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "64",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "65",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "66",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "67",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "68",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "68a",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "69",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "70",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "71",
+        "numImages": 35
+    },
+    {
+        "chapterNumber": "72",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "73",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "74",
+        "numImages": 42
+    },
+    {
+        "chapterNumber": "75",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "76",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "76a",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "77",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "78",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "79",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "80",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "81",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "82",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "83",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "84",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "85",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "86",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "87",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "88",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "89",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "90",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "91",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "92",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "93",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "94",
+        "numImages": 43
+    },
+    {
+        "chapterNumber": "95",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "96",
+        "numImages": 35
+    },
+    {
+        "chapterNumber": "97",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "98",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "99",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "100",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "101",
+        "numImages": 34
+    },
+];
